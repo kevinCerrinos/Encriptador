4a4b56a3d5eaa05ba132eb57275ea8bd15054848
kevin <kcerrinos.23b@gmail.com>
2024-03-18 18:03:06 -0500

    adicion del script para el copy

---
+++ script.js
@@ -0,0 +1,48 @@
+const textArea = document.querySelector('.text-area');
+const mensaje = document.querySelector('.mensaje');
+
+
+function encriptar(cadena){
+  let matriz = [['e','enter'],['i','imes'],['a','ai'],['o','ober'],['u','ufat']]
+  cadena = cadena.toLowerCase()
+
+  for(let i = 0; i < matriz.length; i++){
+    if(cadena.includes(matriz[i][0])){
+      cadena = cadena.replaceAll(matriz[i][0],matriz[i][1])
+    }
+  }
+  return cadena
+}
+
+
+function desencriptar(cadenaencriptada){
+  let matriz = [['e','enter'],['i','imes'],['a','ai'],['o','ober'],['u','ufat']]
+  cadenaencriptada = cadenaencriptada.toLowerCase()
+
+  for(let i = 0; i < matriz.length; i++){
+    if(cadenaencriptada.includes(matriz[i][1])){
+      cadenaencriptada = cadenaencriptada.replaceAll(matriz[i][1],matriz[i][0])
+    }
+  }
+  return cadenaencriptada
+}
+
+
+function btnEncriptar(){
+  const textEncriptado = encriptar(textArea.value)
+  mensaje.value = textEncriptado
+  textArea.value = '';
+  mensaje.style.backgroundImage = "none"
+}
+
+function btnDesencriptar(){
+  const textEncriptado = desencriptar(textArea.value)
+  mensaje.value = textEncriptado
+  textArea.value = '';
+  mensaje.style.backgroundImage = "none"
+}
+
+function btncopiar(){
+  mensaje.select();
+  document.execCommand('copy');
+}
